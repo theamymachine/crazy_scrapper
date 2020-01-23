24fa54c7afe18270eb750fbd956e8c88fa666bf9
theamymachine <amymcarthur10@gmail.com>
2020-01-23 17:30:57 +0100

    dark trader minus test

--- spec/dark_trader_spec.rb
+++ spec/dark_trader_spec.rb
@@ -1,1 +1,6 @@
 require_relative '../lib/dark_trader.rb'
+
+describe "crypto_symbols" do 
+  it "should scrap the symbols of virtual currencies in a webpage and return them in an array"
+  expect (crypto_symbols).to eq ([])
+end 
