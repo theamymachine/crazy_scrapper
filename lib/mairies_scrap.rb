17e4aff533e34fa63cd4612a138acb7af2c29f7c
theamymachine <amymcarthur10@gmail.com>
2020-01-24 01:28:53 +0100

    last mairie try

--- lib/mairies_scrap.rb
+++ lib/mairies_scrap.rb
@@ -1,47 +1,35 @@
+# Gem
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
-#Scrapping townhall url to an array
-def get_townhall_urls
-page = Nokogiri::HTML(open("https://www.annuaire-des-mairies.com/val-d-oise.html"))
-list_url = (page.xpath("//table//td/p/a[@class='lientxt']/@href"))
-list_url_array = []
-list_url.each {|i| list_url_array << i.text}
-list_url_array.map{|url| url[0]="https://www.annuaire-des-mairies.com"}
-return list_url_array
+# Url of page to scrapp
+PAGE_URL = "https://coinmarketcap.com/all/views/all/"
+#Isolate HTML and create array
+def scrapping_page
+  page = Nokogiri::HTML(open(PAGE_URL))
+  # Isolation of HTML element
+  crypto_names = page.xpath("/html/body/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div[3]/div/table/tbody//td[3]/div/text()")
+  crypto_prices = page.xpath("/html/body/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div[3]/div/table/tbody//td[5]//a/text()")
+  # Create array for names
+  crypto_names_array=Array.new
+  crypto_names.each{ |i| crypto_names_array << i.text}
+  # Create array for prices
+  crypto_prices_array=Array.new
+  crypto_prices.each{ |i| crypto_prices_array << i.text}
+  # Return Array with 2 array(names and prices)
+  return[crypto_names_array, crypto_prices_array] 
 end
-#Scrapping email from page
-def get_townhall_email(list_url_array)
-emails_list = []
-list_url_array.each {|generate| page = Nokogiri::HTML(open(generate))
-emails_list << page.xpath("//section[2]/div/table/tbody/tr[4]/td[2]/text()").text}
-return emails_list
+# Create an Hash with the 2 Array
+def create_hashes_in_array
+  # Create a hash of names and prices
+  crypto_hash=Hash[scrapping_page[0].zip scrapping_page[1]]
+  # Create array of hashes
+  final_array=Array.new
+  crypto_hash.each{|key, val|
+    h=Hash.new
+    h[key]=val
+    final_array.push(h)}
+  return final_array
 end
-#Scrapping city name from page
-def get_city_name(list_url_array)
-  city_name_list = []
-  list_url_array.each {|generate| page = Nokogiri::HTML(open(generate))
-  city_name_list << page.xpath("//div/main/section[1]/div/div/div/h1/text()").text}
-  return city_name_list
-end
-#city names from page 
-def get_city_name(list_url_array)
-  city_name_list = []
-  list_url_array.each {|generate| page = Nokogiri::HTML(open(generate))
-  city_name_list << page.xpath("//div/main/section[1]/div/div/div/h1/text()").text}
-  return city_name_list
-end
-#final array with hashes inside 
-def final_table (hash)
-  a = []
-  hash.each{|key, val|
-  h = Hash.new
-  h[key] = val
-  a.push(h)
-  }
-  return a
-end
-
-table = final_table(get_city_name, get_townhall_email)
-
-print table
+# Launcher
+#pp create_hashes_in_array
